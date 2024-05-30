70f55fab28abd4cd1c1b1208073653d66032319f
KATAMII <lydiamwaah01@gmail.com>
2024-05-30 23:47:22 +0300

    write code with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route ,} from 'react-router-dom'
-import Header from './components/header'
-import Home from './pages/Home'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header";
+import Home from "./pages/Home";
 
 function App() {
- 
-
-  return <div className='App'>
-    <Router>
-      <Header />
-      <Routes>
-        <Route path='/' Component={<Home />} />
-      </Routes>
-    </Router>
-  </div>
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" Component={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div className='headerbar'>
-      <div className='logo'>MUT TECH CLUB</div>
-      <div className='navigation'></div>
+    <div className="headerbar">
+      <div className="logo">MUT TECH CLUB</div>
+      <div className="navigation"></div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
