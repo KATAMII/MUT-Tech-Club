1cd563abcf84c8ec3cc43d849f8a0e93c80bea50
KATAMII <lydiamwaah01@gmail.com>
2024-05-30 23:33:09 +0300

    initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route ,} from 'react-router-dom'
+import Header from './components/header'
+import Home from './pages/Home'
+
+function App() {
+ 
+
+  return <div className='App'>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' Component={<Home />} />
+      </Routes>
+    </Router>
+  </div>
+}
+
+export default App
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='headerbar'>
+      <div className='logo'>MUT TECH CLUB</div>
+      <div className='navigation'></div>
+    </div>
+  )
+}
+
+export default Header
